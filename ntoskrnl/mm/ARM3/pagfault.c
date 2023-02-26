195c61898bc8f7dc7c1cf4d218a6ddaea40a9d7d
Shilin Victor <chrono.monochrome@gmail.com>
2023-02-26 00:17:08 +0000

    Revert "[NTOS:MM] Do not zero-fill pages when not needed"
    This reverts commit 708dc6b7d6c28447279025480885e3f608224694.

--- ntoskrnl/mm/ARM3/pagfault.c
+++ ntoskrnl/mm/ARM3/pagfault.c
@@ -674,11 +674,6 @@ MiResolveDemandZeroFault(IN PVOID Address,
             PageFrameNumber = MiRemoveAnyPage(Color);
             NeedZero = TRUE;
         }
-        else
-        {
-            /* Page guaranteed to be zero-filled */
-            NeedZero = FALSE;
-        }
     }
     else
     {
@@ -693,8 +688,6 @@ MiResolveDemandZeroFault(IN PVOID Address,
         {
             /* System wants a zero page, obtain one */
             PageFrameNumber = MiRemoveZeroPage(Color);
-            /* No need to zero-fill it */
-            NeedZero = FALSE;
         }
     }
 
