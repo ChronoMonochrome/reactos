546ad17ca5b81532ac131bf4691473789a2ec3c9
Shilin Victor <chrono.monochrome@gmail.com>
2023-02-26 00:22:22 +0000

    Revert "[NTOS:MM] Acquire file lock when creating section"
    This reverts commit 7d70af61e8fff18b24d9baa3bb63c37c96aa21c6.

--- ntoskrnl/mm/section.c
+++ ntoskrnl/mm/section.c
@@ -4291,7 +4291,6 @@ MmCreateSection (OUT PVOID  * Section,
     NTSTATUS Status;
     ULONG Protection;
     PSECTION *SectionObject = (PSECTION *)Section;
-    BOOLEAN FileLock = FALSE;
 
     /* Check if an ARM3 section is being created instead */
     if (!(AllocationAttributes & (SEC_IMAGE | SEC_PHYSICALMEMORY)))
@@ -4345,24 +4344,6 @@ MmCreateSection (OUT PVOID  * Section,
                 DPRINT1("Failed to get a handle to the FO: %lx\n", Status);
                 return Status;
             }
-
-            /* Lock the file */
-            Status = FsRtlAcquireToCreateMappedSection(FileObject, SectionPageProtection);
-            if (!NT_SUCCESS(Status))
-            {
-                ObDereferenceObject(FileObject);
-                return Status;
-            }
-
-            FileLock = TRUE;
-
-            /* Deny access if there are writes on the file */
-            if ((AllocationAttributes & SEC_IMAGE) && (Status == STATUS_FILE_LOCKED_WITH_WRITERS))
-            {
-                DPRINT1("Cannot create image maps with writers open on the file!\n");
-                Status = STATUS_ACCESS_DENIED;
-                goto Quit;
-            }
         }
         else
         {
@@ -4385,6 +4366,7 @@ MmCreateSection (OUT PVOID  * Section,
                                       SectionPageProtection,
                                       AllocationAttributes,
                                       FileObject);
+        ObDereferenceObject(FileObject);
     }
 #ifndef NEWCC
     else if (FileObject != NULL)
@@ -4397,6 +4379,7 @@ MmCreateSection (OUT PVOID  * Section,
                                           AllocationAttributes,
                                           FileObject,
                                           FileHandle != NULL);
+        ObDereferenceObject(FileObject);
     }
 #else
     else if (FileHandle != NULL || FileObject != NULL)
@@ -4414,13 +4397,9 @@ MmCreateSection (OUT PVOID  * Section,
     {
         /* All cases should be handled above */
         Status = STATUS_INVALID_PARAMETER;
+        if (FileObject)
+            ObDereferenceObject(FileObject);
     }
-
-Quit:
-    if (FileLock)
-        FsRtlReleaseFile(FileObject);
-    if (FileObject)
-        ObDereferenceObject(FileObject);
 
     return Status;
 }
