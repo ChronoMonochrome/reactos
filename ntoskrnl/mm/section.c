b55f77a828cdf50420f42a73e50970cb7f3dc9ba
Shilin Victor <chrono.monochrome@gmail.com>
2023-02-26 00:22:26 +0000

    Revert "[NTOS/MM] Force flushing when the page is dirtified in user mode"
    This reverts commit 7dab3583cc4c80304df61658fe4b1eeea8983f5e.

--- ntoskrnl/mm/section.c
+++ ntoskrnl/mm/section.c
@@ -4621,7 +4621,7 @@ MmRosFlushVirtualMemory(
         if (Entry != 0)
         {
             /* This will write the page to disk, if needed */
-            MmCheckDirtySegment(Segment, &SegmentOffset, Process ? MmIsDirtyPage(Process, CurrentAddress) : FALSE, FALSE);
+            MmCheckDirtySegment(Segment, &SegmentOffset, MmIsDirtyPage(Process, CurrentAddress), FALSE);
             Iosb->Information += PAGE_SIZE;
         }
         SegmentOffset.QuadPart += PAGE_SIZE;
@@ -4695,10 +4695,7 @@ MmCheckDirtySegment(
 
         /* Drop the reference we got */
         Entry = MAKE_SSE(Page << PAGE_SHIFT, SHARE_COUNT_FROM_SSE(Entry) - 1);
-        if (DirtyAgain)
-        {
-            Entry = DIRTY_SSE(Entry);
-        }
+        if (DirtyAgain) Entry = DIRTY_SSE(Entry);
         MmSetPageEntrySectionSegment(Segment, Offset, Entry);
     }
 
