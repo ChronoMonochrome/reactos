1ced98432ebc92cce2e8e30624659375e56ae108
Shilin Victor <chrono.monochrome@gmail.com>
2023-02-26 00:18:02 +0000

    Revert "[NTOS:MM] Fix PFN lock usage"
    This reverts commit 33949028fbceb25eedf9d71475ccfc4be063a6cd.

--- ntoskrnl/mm/freelist.c
+++ ntoskrnl/mm/freelist.c
@@ -566,9 +566,10 @@ NTAPI
 MmDereferencePage(PFN_NUMBER Pfn)
 {
     PMMPFN Pfn1;
+    KIRQL OldIrql;
     DPRINT("MmDereferencePage(PhysicalAddress %x)\n", Pfn << PAGE_SHIFT);
 
-    MI_ASSERT_PFN_LOCK_HELD();
+    OldIrql = MiAcquirePfnLock();
 
     Pfn1 = MiGetPfnEntry(Pfn);
     ASSERT(Pfn1);
@@ -595,6 +596,8 @@ MmDereferencePage(PFN_NUMBER Pfn)
         DPRINT("Legacy free: %lx\n", Pfn);
         MiInsertPageInFreeList(Pfn);
     }
+
+    MiReleasePfnLock(OldIrql);
 }
 
 PFN_NUMBER
