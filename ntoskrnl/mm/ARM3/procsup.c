4a631b34031bfc1695acd136f17dd33c1a30435b
Shilin Victor <chrono.monochrome@gmail.com>
2023-02-26 00:17:09 +0000

    Revert "[NTOS:MM] Copy the WorkingSet page when initializing "hand-built" process"
    This reverts commit 34662e2f6fa556855759978df306718dfb9f0930.

--- ntoskrnl/mm/ARM3/procsup.c
+++ ntoskrnl/mm/ARM3/procsup.c
@@ -1108,7 +1108,6 @@ MmInitializeHandBuiltProcess(IN PEPROCESS Process,
 
     /* Use idle process Working set */
     Process->Vm.VmWorkingSetList = PsGetCurrentProcess()->Vm.VmWorkingSetList;
-    Process->WorkingSetPage = PsGetCurrentProcess()->WorkingSetPage;
 
     /* Done */
     Process->HasAddressSpace = TRUE;//??
