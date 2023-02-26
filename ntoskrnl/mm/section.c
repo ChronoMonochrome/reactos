f3829dbff2b3d82aa58f7c8d9efd61b24920ce5e
Shilin Victor <chrono.monochrome@gmail.com>
2023-02-26 00:22:27 +0000

    Revert "[NTOS:MM] Do not limit ourselves in reading or writing the file"
    This reverts commit 91edefa11e4d7230155dcaafe052a6fd10d7dd57.

--- ntoskrnl/mm/section.c
+++ ntoskrnl/mm/section.c
@@ -215,8 +215,15 @@ MiWritePage(PMM_SECTION_SEGMENT Segment,
     PMDL Mdl = (PMDL)MdlBase;
     PFILE_OBJECT FileObject = Segment->FileObject;
     LARGE_INTEGER FileOffset;
+    PFSRTL_COMMON_FCB_HEADER Fcb = FileObject->FsContext;
 
     FileOffset.QuadPart = Segment->Image.FileOffset + SegOffset;
+
+    /* Check if we are not writing off-limit */
+    if (FileOffset.QuadPart >= Fcb->AllocationSize.QuadPart)
+    {
+        return STATUS_SUCCESS;
+    }
 
     RtlZeroMemory(MdlBase, sizeof(MdlBase));
     MmInitializeMdl(Mdl, NULL, PAGE_SIZE);
@@ -1173,14 +1180,17 @@ MiReadPage(PMEMORY_AREA MemoryArea,
     PMDL Mdl = (PMDL)MdlBase;
     PFILE_OBJECT FileObject = MemoryArea->SectionData.Segment->FileObject;
     LARGE_INTEGER FileOffset;
+    PFSRTL_COMMON_FCB_HEADER Fcb = FileObject->FsContext;
 
     FileOffset.QuadPart = MemoryArea->SectionData.Segment->Image.FileOffset + SegOffset;
-
-    DPRINT("Reading file at offset %08x:%08x\n", FileOffset.HighPart, FileOffset.LowPart);
 
     Status = MmRequestPageMemoryConsumer(MC_USER, FALSE, Page);
     if (!NT_SUCCESS(Status))
         return Status;
+
+    /* Check if we are beyond the file */
+    if (FileOffset.QuadPart > Fcb->FileSize.QuadPart)
+        return STATUS_SUCCESS;
 
     RtlZeroMemory(MdlBase, sizeof(MdlBase));
     MmInitializeMdl(Mdl, NULL, PAGE_SIZE);
@@ -1195,7 +1205,6 @@ MiReadPage(PMEMORY_AREA MemoryArea,
         KeWaitForSingleObject(&Event, WrPageIn, KernelMode, FALSE, NULL);
         Status = IoStatus.Status;
     }
-
     if (Mdl->MdlFlags & MDL_MAPPED_TO_SYSTEM_VA)
     {
         MmUnmapLockedPages (Mdl->MappedSystemVa, Mdl);
@@ -4531,7 +4540,6 @@ MmMakePagesResident(
             MmUnlockAddressSpace(AddressSpace);
 
             /* FIXME: Read the whole range at once instead of one page at a time */
-            /* Ignore file size, as Cc already checked on its side. */
             Status = MiReadPage(MemoryArea, SegmentOffset.QuadPart, &Page);
             if (!NT_SUCCESS(Status))
             {
